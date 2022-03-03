9dcff0809b04193aa29f99550fc6faa65361ca23
jiajoestar <100849096+jiajoestar@users.noreply.github.com>
2022-03-03 12:59:57 +0000

    Add files via upload

---
+++ frogger.py
@@ -0,0 +1,16 @@
+from pygame import *
+
+init()
+
+width = 1200
+height = 1200
+screen = display.set_mode((width, height))
+
+gameOver = False
+score = 0
+
+playerImage = image.load("kirby.png")
+playerImage = transform.scale(playerImage, (40,40))
+obstacle1 = 
+obstacle2 = 
+
