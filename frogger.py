002b960a979de377f3ed9ccc6fb3dec1748c5aaa
jiajoestar <100849096+jiajoestar@users.noreply.github.com>
2022-03-04 15:05:28 +0000

    Update frogger.py

--- frogger.py
+++ frogger.py
@@ -11,6 +11,24 @@ score = 0
 
 playerImage = image.load("kirby.png")
 playerImage = transform.scale(playerImage, (40,40))
-obstacle1 = 
-obstacle2 = 
+obstacle1 = image.load()
+obstacle2 = image.load()
+
+# set up player
+px = 0
+player = Rect(600,600,100,100)
+
+# setting up obstacles
+
+while not gameOver:
+  for e in event.get():
+    if e.type == QUIT:
+      gameOver = True
+    elif e.type == KEYDOWN:
+      if e.key == K_LEFT:
+        px = -3
+      if e.key == K_RIGHT:
+        px = 3
+      if e.key == K_
+
 
