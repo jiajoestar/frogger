359683d7a3089c7daaec7fbcf11a2f248b8791e0
jiajoestar <100849096+jiajoestar@users.noreply.github.com>
2022-03-29 20:54:02 +0100

    Add files via upload

--- frogger.py
+++ frogger.py
@@ -3,10 +3,10 @@ from pygame import *
 init()
 
 width = 500
-height = 800
+height = 600
 screen = display.set_mode((width, height))
-background = (0,0,0)
-river = (30,144,255)
+background = image.load("background.png")
+background = transform.scale(background, (500,600))
 
 score = 0
 
@@ -14,132 +14,77 @@ playerImage = image.load("kirby.png")
 playerImage = transform.scale(playerImage, (80,40))
 obstacle1 = image.load("car.png")
 obstacle1 = transform.scale(obstacle1, (40,20))
-obstacle2 = image.load("log.png")
-obstacle2 = transform.scale(obstacle2, (60,10))
-obstacle3 = image.load("turtles.png")
-obstacle3 = transform.scale(obstacle3, (40,40))
+obstacle2 = image.load("car2.png")
+obstacle2 = transform.scale(obstacle2, (40,20))
+obstacle3 = image.load("log.png")
+obstacle3 = transform.scale(obstacle3, (60,20))
+obstacle4 = image.load("turtles.png")
+obstacle4 = transform.scale(obstacle4, (40,40))
 
 # creating obstacles
 def createCars():
 	cars = []
 	x = 0
 	while len(cars) < 3:
+		cars.append(Rect(x,480,20,10))
 		cars.append(Rect(x,400,20,10))
 		x += 150
 		
 	return cars
 	
-#def createCars2():
-#	cars2 = []
-#	x = 0
-#	while len(cars2) < 3:
-#		cars2.append(Rect(x,300,20,10))
-#		x += 150
-#	return cars2
 
 # drawing cars
 def drawCars(cars, screen):
 	for i in cars:
 		screen.blit(obstacle1, i)
+		screen.blit(obstacle2, i)
 		
-#def drawCars2(cars2, screen):
-#	for i in cars2:
-#		screen.blit(obstacle1, i)
 
-'''
-# moving cars
-def moveCars(cars, dx):
-	for i in cars:
-		if i.right > width or i.left < 0:
-			dx = dx * -1
-			for car in cars:
-				car.move_ip(0,50)
-			break
-				
-	for i in car:
-		i.move_ip(dx,0)
-	
-	return dx
-	'''
 # creating turtles
 def createTurtles():
 	turtles = []
-	y = 50
-	num_rows = 2
-	num_turtles_rows = 3
+	x = 0
 	
-	while y <= 50 * num_rows: 
-		x = 30 
-		while x <= 30 * num_turtles_rows:
-			turtles.append(Rect(x,y,40,40))
-			x = x + 400
+	while len(turtles) < 3: 
+		turtles.append(Rect(x,150,40,40))
+		x += 200
 		
 	return turtles 
 
 # drawing turtles
 def drawTurtles(turtles, screen):
 	for i in turtles:
-		screen.blit(obstacle3, i)
+		screen.blit(obstacle4, i)
 
-# moving turtles
-#def moveTurtles(turtles, dx):
-#	for i in turtles:
-#		if i.right > width or i.left < 0:
-#			dx = dx * -1
-#			for turtle in turtles:
-#				turtles.move_ip(0,50)
-#			break
-				
-#	for i in turtle:
-#		i.move_ip(dx,0)
-	
-#	return dx
 
 # creating logs
 def createLogs():
 	logs = []
-	y = 20
-	num_rows = 2
-	num_logs_rows = 2
+	x = 0
 	
-	while y <= 20 * num_rows: 
-		x = 10 
-		while x <= 10 * num_logs_rows:
-			logs.append(Rect(x,y,60,10))
-			x = x + 400
+	while len(logs) < 4:
+		logs.append(Rect(x,200,60,20))
+		logs.append(Rect(x,250,60,20))
+		x += 250
 		
 	return logs
 	
 # drawing logs
 def drawLogs(logs, screen):
 	for i in logs:
-		screen.blit(obstacle2, i)
-
-# moving logs
-#def moveLogs(logs, dx):
-#	for i in logs:
-#		if i.right > width or i.left < 0:
-#			dx = dx * -1
-#			for log in logs:
-#				log.move_ip(0,50)
-#			break
-#				
-#	for i in log:
-#		i.move_ip(dx,0)
-#	
-#	return dx
+		screen.blit(obstacle3, i)
 
 # set up player
 px = 20
 py = 20
+
 # setting up obstacles
 cars = createCars()
-#cars2 = createCars2()
 turtles = createTurtles()
 logs = createLogs()
 dx = 2
-#framecount = 0
-player = Rect(250,600,80,40)
+player = Rect(220,500,80,40)
+
 # game loop
 gameOver = False
 while gameOver == False:
@@ -155,8 +100,7 @@ while gameOver == False:
 				player.move_ip(0,-py)
 				
 	# draw background
-	screen.fill(background)
-	#draw.rect(screen, river, (0,250,500,500))
+	screen.blit(background, (0,0))
 	
 	for i in cars:
 		i.move_ip(dx,0)
@@ -166,56 +110,48 @@ while gameOver == False:
 	for i in cars:
 		if i.x >= width:
 			cars.remove(i)
-			cars.append(Rect(0,400,20,10))
+			cars.append(Rect(0,480,20,10))
+			cars.append(Rect(0,400,20,10))	
+			
 			
 	for i in cars:
 		if player.colliderect(i):
-			player.update(250,600,80,40)
+			player.update(220,500,80,40)
+	
+	for j in turtles:
+		j.move_ip(-dx,0)
+		screen.blit(obstacle4,j)
+		
+	for j in turtles:
+		if j.x <= 0:
+			turtles.remove(j)
+			turtles.append(Rect(width,150,40,40))
+	
+	for j in turtles:
+		if player.colliderect(j):
+			player.update(220,500,60,40)
+	
+	for k in logs:
+		k.move_ip(dx,0)
+		screen.blit(obstacle3,k)
+		
+	for k in logs:
+		if k.x >= width:
+			logs.remove(k)
+			logs.append(Rect(0,200,60,20))
+			logs.append(Rect(0,250,60,20))
 			
-#	for i in cars2:
-#		i.move_ip(-dx,0)
-#		screen.blit(obstacle1,i)
-#		
-#	for i in cars2:
-#		if i.x <= width:
-#			cars2.remove(i)
-#			cars2.append(Rect(0,300,20,10))		
-#	
-#			
-#	for i in turtles:
-#		i.move_ip(dx,0)
-#		screen.blit(obstacle3,i)
-#	
-#	for i in turtles:
-#		if i.x >= width:
-#			turtles.remove(i)
-#			turtles.append(Rect(0,100,40,20))
-#			
-#	
-#	for i in logs:
-#		i.move_ip(dx,0)
-#		screen.blit(obstacle2,i)
-#		
-#		
+	for k in logs:
+		if player.colliderect(k):
+			player.update(220,500,60,40)
+	
+			
+		
 	screen.blit(playerImage, player)
 	
-	
-		
-	#drawCars = (cars, screen)
-	
-	
-	
-				
-#	if framecount >= 100:
-#		dx = moveCars(cars, dx)
-#		framecount = 0
-#	framecount = framecount + 1
 	# move player
 	display.flip()
 	time.delay(10)
 	
  
-#
-
-
 
